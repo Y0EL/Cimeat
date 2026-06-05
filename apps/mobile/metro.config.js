16659d7841482531fb49f89ff4c079a063440a79
Y0EL <yoelandreasmanoppo@gmail.com>
2026-06-05 13:19:46 +0700

    fix: metro config monorepo watchFolders + safe-area-context correct version

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -6,11 +6,10 @@ const monorepoRoot = path.resolve(projectRoot, '../..')
 
 const config = getDefaultConfig(projectRoot)
 
-config.watchFolders = [monorepoRoot]
+config.watchFolders = [...(config.watchFolders ?? []), monorepoRoot]
 config.resolver.nodeModulesPaths = [
   path.resolve(projectRoot, 'node_modules'),
   path.resolve(monorepoRoot, 'node_modules'),
 ]
-config.resolver.disableHierarchicalLookup = true
 
 module.exports = config
